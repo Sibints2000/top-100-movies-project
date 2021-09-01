88afe9f761421c7f0c0b5d702a3bfe3f1edc063e
Sibints2000 <sibints2000@gmail.com>
2021-09-01 20:10:09 +0530

    Twenty First Commit

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ all_movies = soup.find_all(name="h3", class_="jsx-4245974604")
 movie_titles = [movie.getText() for movie in all_movies]
 movies = movie_titles[::-1]
 
-with open(movies.txt, mode="w") as file:
+with open("movies.txt", mode="w") as file:
     for movie in movies:
-        file.write()
+        file.write(f"{movie}\n")
